f2c1d9c306379f6c4fe137d70a2f9ca24dec3149
Alish <alishbaig2004@gmail.com>
2024-04-10 21:13:57 +0530

    feat: Add login and register pages and components

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LoginForm } from "../_components/login-form";
+
+function LoginPage() {
+  return (
+    <div className="w-full h-screen grid place-items-center bg-long-bars">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RegisterForm } from "../_components/register-form";
+
+function RegisterPage() {
+  return (
+    <div className="w-full h-screen grid place-items-center bg-long-bars">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default RegisterPage;
