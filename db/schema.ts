61f66c9f1d083198a16aff78c57180cab0769d46
Alish Baig <alishbaig2004@gmail.com>
2024-04-10 08:45:51 +0530

    feat: Create database schema and add new tables and relations

--- db/schema.ts
+++ db/schema.ts
@@ -1,80 +1,171 @@
-import { like, relations } from "drizzle-orm";
-import { PgArray, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  index,
+  integer,
+  pgTable,
+  primaryKey,
+  serial,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { init } from "@paralleldrive/cuid2";
 
-export const userTable = pgTable("user", {
-  id: text("id").primaryKey(),
-  name: text("name"),
-  username: text("username").unique().notNull(),
-  email: text("email").unique().notNull(),
-  password: text("password").notNull(),
-  avatar: text("avatar"),
-  bio: text("bio"),
-  link: text("link"),
-
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
+const createId = init({
+  random: Math.random,
+  length: 8,
 });
 
-export const sessionTable = pgTable("session", {
+export const users = pgTable(
+  "user",
+  {
+    id: uuid("id").primaryKey(),
+    email: text("email").unique().notNull(),
+    username: text("username").unique().notNull(),
+    password: text("password").notNull(),
+
+    name: text("name"),
+    avatar: text("avatar"),
+    bio: text("bio"),
+    link: text("link"),
+
+    createdAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at").notNull(),
+  },
+  (table) => ({
+    usernameIdx: index("username").on(table.username),
+  })
+);
+
+export const account = pgTable(
+  "account",
+  {
+    userId: text("user_id")
+      .notNull()
+      .references(() => users.id),
+    providerId: text("provider_id").notNull(),
+    providerUserId: text("provider_user_id").notNull(),
+    expiresAt: integer("expires_at"),
+    scope: text("scope"),
+  },
+  (table) => ({
+    compoundKey: primaryKey({
+      columns: [table.providerId, table.providerUserId],
+    }),
+  })
+);
+
+export const sessions = pgTable("session", {
   id: text("id").primaryKey(),
   userId: text("user_id")
     .notNull()
-    .references(() => userTable.id),
+    .references(() => users.id),
   expiresAt: timestamp("expires_at", {
     withTimezone: true,
     mode: "date",
   }).notNull(),
 });
 
-export const postTable = pgTable("post", {
-  id: text("id").primaryKey(),
-  userId: text("user_id"),
-  body: text("body").notNull(),
-  image: text("image"),
-  likeIds: text("like_ids").array(),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
-});
+export const posts = pgTable(
+  "post",
+  {
+    id: varchar("id", { length: 8 })
+      .primaryKey()
+      .notNull()
+      .$defaultFn(() => createId()),
+    userId: text("user_id"),
+    parentId: varchar("parent_id", { length: 8 }),
 
-export const commentTable = pgTable("comment", {
-  id: text("id").primaryKey(),
-  userId: text("user_id")
-    .notNull()
-    .references(() => userTable.id),
-  postId: text("post_id")
-    .notNull()
-    .references(() => postTable.id),
-  body: text("body").notNull(),
-  likeIds: text("like_ids").array(),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
-});
+    body: text("body").notNull(),
+    image: text("image"),
 
-export const hashTagTable = pgTable("hash_tag", {
+    createdAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+  },
+  (table) => ({
+    userIdx: index("userId").on(table.userId),
+  })
+);
+
+export const likes = pgTable(
+  "like",
+  {
+    id: serial("id").primaryKey().notNull(),
+    userId: text("user_id").notNull(),
+    postId: varchar("post_id", { length: 8 }).notNull(),
+
+    createdAt: timestamp("created_at").notNull(),
+  },
+  (table) => ({
+    postIdx: index("postId").on(table.postId),
+  })
+);
+
+export const hashTags = pgTable("hash_tag", {
   id: text("id").primaryKey(),
   name: text("name").unique().notNull(),
   userId: text("user_id").notNull(),
-  postIds: text("post_ids").array(),
+  postIds: text("post_ids"),
   createdAt: timestamp("created_at").notNull(),
   updatedAt: timestamp("updated_at").notNull(),
 });
 
-export const postRelation = relations(postTable, ({ one, many }) => ({
-  author: one(userTable, {
-    fields: [postTable.userId],
-    references: [userTable.id],
+export const followers = pgTable(
+  "follower",
+  {
+    id: serial("id").primaryKey().notNull(),
+    userId: text("user_id").notNull(),
+    followerId: text("follower_id").notNull(),
+    createdAt: timestamp("created_at").notNull(),
+  },
+  (table) => ({
+    userFollowerIdx: index("user_follower").on(table.userId, table.followerId),
+  })
+);
+
+// Post Relations
+
+export const postRelations = relations(posts, ({ one, many }) => ({
+  user: one(users, {
+    fields: [posts.userId],
+    references: [users.id],
   }),
-  comments: many(commentTable),
-  hashTagTable: many(hashTagTable),
+
+  likes: many(likes),
+  replies: many(posts, {
+    relationName: "replies",
+  }),
+  parent: one(posts, {
+    fields: [posts.parentId],
+    references: [posts.id],
+    relationName: "replies",
+  }),
 }));
 
-export const commentRelation = relations(commentTable, ({ one }) => ({
-  author: one(userTable, {
-    fields: [commentTable.userId],
-    references: [userTable.id],
+// User Relation
+
+export const userRelation = relations(users, ({ many }) => ({
+  posts: many(posts),
+  likes: many(likes),
+  followers: many(followers),
+  likedPosts: many(likes),
+}));
+
+export const likesRelation = relations(likes, ({ one }) => ({
+  user: one(users, {
+    fields: [likes.userId],
+    references: [users.id],
   }),
-  post: one(postTable, {
-    fields: [commentTable.postId],
-    references: [postTable.id],
+  post: one(posts, {
+    fields: [likes.postId],
+    references: [posts.id],
+  }),
+}));
+
+export const followerRelation = relations(followers, ({ one }) => ({
+  user: one(users, {
+    fields: [followers.userId],
+    references: [users.id],
   }),
 }));
