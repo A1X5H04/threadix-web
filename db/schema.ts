ccb9e977bbd785f2f7d2978b27731d4bd128d95f
Alish <alishbaig2004@gmail.com>
2024-04-13 15:40:53 +0530

    feat: Refactor database migration and schema files

--- db/schema.ts
+++ db/schema.ts
@@ -67,105 +67,101 @@ export const sessions = pgTable("session", {
   }).notNull(),
 });
 
-export const posts = pgTable(
-  "post",
-  {
-    id: varchar("id", { length: 8 })
-      .primaryKey()
-      .notNull()
-      .$defaultFn(() => createId()),
-    userId: text("user_id"),
-    parentId: varchar("parent_id", { length: 8 }),
+// export const posts = pgTable(
+//   "post",
+//   {
+//     id: varchar("id", { length: 8 })
+//       .primaryKey()
+//       .notNull()
+//       .$defaultFn(() => createId()),
+//     userId: text("user_id"),
+//     parentId: varchar("parent_id", { length: 8 }),
 
-    body: text("body").notNull(),
-    image: text("image"),
+//     body: text("body").notNull(),
+//     image: text("image"),
 
-    createdAt: timestamp("created_at").notNull(),
-    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
-  },
-  (table) => ({
-    userIdx: index("userId").on(table.userId),
-  })
-);
+//     createdAt: timestamp("created_at").notNull(),
+//     updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+//   },
+//   (table) => ({
+//     userIdx: index("userId").on(table.userId),
+//   })
+// );
 
-export const likes = pgTable(
-  "like",
-  {
-    id: serial("id").primaryKey().notNull(),
-    userId: text("user_id").notNull(),
-    postId: varchar("post_id", { length: 8 }).notNull(),
+// export const likes = pgTable(
+//   "like",
+//   {
+//     id: serial("id").primaryKey().notNull(),
+//     userId: text("user_id").notNull(),
+//     postId: varchar("post_id", { length: 8 }).notNull(),
 
-    createdAt: timestamp("created_at").notNull(),
-  },
-  (table) => ({
-    postIdx: index("postId").on(table.postId),
-  })
-);
+//     createdAt: timestamp("created_at").notNull(),
+//   },
+//   (table) => ({
+//     postIdx: index("postId").on(table.postId),
+//   })
+// );
 
-export const hashTags = pgTable("hash_tag", {
-  id: text("id").primaryKey(),
-  name: text("name").unique().notNull(),
-  userId: text("user_id").notNull(),
-  postIds: text("post_ids"),
-  createdAt: timestamp("created_at").notNull(),
-  updatedAt: timestamp("updated_at").notNull(),
-});
+// export const hashTags = pgTable("hash_tag", {
+//   id: text("id").primaryKey(),
+//   name: text("name").unique().notNull(),
+//   userId: text("user_id").notNull(),
+//   postIds: text("post_ids"),
+//   createdAt: timestamp("created_at").notNull(),
+//   updatedAt: timestamp("updated_at").notNull(),
+// });
 
-export const followers = pgTable(
-  "follower",
-  {
-    id: serial("id").primaryKey().notNull(),
-    userId: text("user_id").notNull(),
-    followerId: text("follower_id").notNull(),
-    createdAt: timestamp("created_at").notNull(),
-  },
-  (table) => ({
-    userFollowerIdx: index("user_follower").on(table.userId, table.followerId),
-  })
-);
+// export const followers = pgTable(
+//   "follower",
+//   {
+//     id: serial("id").primaryKey().notNull(),
+//     userId: text("user_id").notNull(),
+//     followerId: text("follower_id").notNull(),
+//     createdAt: timestamp("created_at").notNull(),
+//   },
+//   (table) => ({
+//     userFollowerIdx: index("user_follower").on(table.userId, table.followerId),
+//   })
+// );
 
-// Post Relations
+// export const postRelations = relations(posts, ({ one, many }) => ({
+//   user: one(users, {
+//     fields: [posts.userId],
+//     references: [users.id],
+//   }),
 
-export const postRelations = relations(posts, ({ one, many }) => ({
-  user: one(users, {
-    fields: [posts.userId],
-    references: [users.id],
-  }),
+//   likes: many(likes),
+//   replies: many(posts, {
+//     relationName: "replies",
+//   }),
+//   parent: one(posts, {
+//     fields: [posts.parentId],
+//     references: [posts.id],
+//     relationName: "replies",
+//   }),
+// }));
 
-  likes: many(likes),
-  replies: many(posts, {
-    relationName: "replies",
-  }),
-  parent: one(posts, {
-    fields: [posts.parentId],
-    references: [posts.id],
-    relationName: "replies",
-  }),
-}));
+// export const userRelation = relations(users, ({ many }) => ({
+//   posts: many(posts),
+//   likes: many(likes),
+//   followers: many(followers),
+//   likedPosts: many(likes),
+// }));
 
-// User Relation
+// export const likesRelation = relations(likes, ({ one }) => ({
+//   user: one(users, {
+//     fields: [likes.userId],
+//     references: [users.id],
+//   }),
+//   post: one(posts, {
+//     fields: [likes.postId],
+//     references: [posts.id],
+//   }),
+// }));
 
-export const userRelation = relations(users, ({ many }) => ({
-  posts: many(posts),
-  likes: many(likes),
-  followers: many(followers),
-  likedPosts: many(likes),
-}));
-
-export const likesRelation = relations(likes, ({ one }) => ({
-  user: one(users, {
-    fields: [likes.userId],
-    references: [users.id],
-  }),
-  post: one(posts, {
-    fields: [likes.postId],
-    references: [posts.id],
-  }),
-}));
-
-export const followerRelation = relations(followers, ({ one }) => ({
-  user: one(users, {
-    fields: [followers.userId],
-    references: [users.id],
-  }),
-}));
+// export const followerRelation = relations(followers, ({ one }) => ({
+//   user: one(users, {
+//     fields: [followers.userId],
+//     references: [users.id],
+//   }),
+// }));
