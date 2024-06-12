62f8357b1a3c8d1ff60575db8137023a0f9425d7
Alish <alishbaig2004@gmail.com>
2024-06-12 13:36:32 +0530

    refactor: Update activity page to display "No Activity Found" message

--- app/(main)/(routes)/activity/page.tsx
+++ app/(main)/(routes)/activity/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function ActivityPage() {
-  return <div>ActivityPage</div>;
+  return (
+    <div className="grid place-items-center w-full">
+      <div className="grid place-items-center">
+        <h4 className="font-semibold text-lg">No Activity Found</h4>
+        <p className="text-sm text-muted-foreground">
+          When you like or follow something, it will show up here.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ActivityPage;
