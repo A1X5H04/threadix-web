33ef9e2530b0bf636ec8f020ea8bde4ca9d5e1a8
Alish <alishbaig2004@gmail.com>
2024-06-12 13:29:52 +0530

    feat: Update database schema file path to include all schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 
 export default {
   driver: "pg",
-  schema: "./db/schema.ts",
+  schema: "./db/schemas/*",
   out: "./db/migrations",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
