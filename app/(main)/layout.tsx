abb51499e571d0659e5a5f8b5ad3a8c601bd04f5
Alish <alishbaig2004@gmail.com>
2024-06-12 13:36:19 +0530

    refactor: Add user authentication to main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,16 @@
 import Navbar from "@/components/navbar";
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 
-function MainLayout({ children }: Props) {
+async function MainLayout({ children }: Props) {
+  const { user } = await validateRequest();
+
+  if (!user) return redirect("/login");
   return (
     <main className="max-w-2xl mx-auto px-5">
       <Navbar />
