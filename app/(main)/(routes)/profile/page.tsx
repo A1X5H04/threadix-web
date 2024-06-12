6b6f5f7d71e24405cb9dd907d0fac464e6cad65e
Alish <alishbaig2004@gmail.com>
2024-06-12 13:36:12 +0530

    refactor: Update profile page to display user information dynamically

--- app/(main)/(routes)/profile/page.tsx
+++ app/(main)/(routes)/profile/page.tsx
@@ -1,57 +1,77 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { RiMoreFill } from "@remixicon/react";
+import { validateRequest } from "@/lib/auth";
+import { RiLockFill, RiMoreFill } from "@remixicon/react";
+import { redirect } from "next/navigation";
 import React from "react";
+import { twMerge } from "tailwind-merge";
+import { EditDialog } from "./_components/edit-dialog";
+import FollowDialog from "./_components/follow-dialog";
 
-function ProfilePage() {
+async function ProfilePage() {
+  const { user } = await validateRequest();
+
+  if (!user) return redirect("/login");
+
   return (
     <div className="px-5">
       <div className="flex items-center justify-between">
         <div className="inline-flex flex-col">
-          <h4 className="text-xl font-semibold">Name</h4>
-          <span className="text-muted-foreground text-sm">username</span>
+          <h4 className="text-xl font-bold">{user.name}</h4>
+          {/* <RiLockFill className="w-4 h-4 text-muted-foreground" /> */}
+          <span className="text-muted-foreground text-sm">{user.username}</span>
         </div>
         <div>
           <Avatar className="w-16 h-16">
-            <AvatarImage src="https://api.dicebear.com/8.x/lorelei-neutral/svg" />
-            <AvatarFallback>N</AvatarFallback>
+            <AvatarImage src={user.avatar} />
+            <AvatarFallback className="uppercase">
+              {user.name.at(0)}
+            </AvatarFallback>
           </Avatar>
         </div>
       </div>
-      <div className="mt-5 space-y-3">
-        <p className="text-[15px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A similique
-          laboriosam molestias ea dolore vel accusamus aut magni nulla vero?
+      <div className="mt-3.5 space-y-3">
+        <p
+          className={twMerge(
+            "text-[15px]",
+            !user.bio && "text-sm italic text-muted-foreground"
+          )}
+        >
+          {user.bio || `${user.name} has not set a bio yet.`}
         </p>
         <div className="flex items-center justify-between gap-x-2">
           <div className="inline-flex items-center gap-x-2">
             <Button variant="link" className="px-0 text-muted-foreground">
-              10 Followers
+              No Followers
             </Button>
-            •
-            <Button variant="link" className="px-0 text-muted-foreground">
-              https://example.com
-            </Button>
+            -
+            <FollowDialog />
+            {user.link && (
+              <Button variant="link" className="px-0 text-muted-foreground">
+                {user.link}
+              </Button>
+            )}
           </div>
           <Button variant="ghost" size="icon">
             <RiMoreFill />
           </Button>
         </div>
         <div>
-          <Button variant="outline" className="w-full">
-            Edit Profile
-          </Button>
+          <EditDialog />
         </div>
       </div>
-      <div className="mt-4">
-        <Tabs defaultValue="account">
-          <TabsList className="grid w-full grid-cols-2">
-            <TabsTrigger value="account">Posts</TabsTrigger>
-            <TabsTrigger value="password">Replies</TabsTrigger>
-            <TabsContent value="account">asdfasd</TabsContent>
-            <TabsContent value="password">rreete</TabsContent>
+      <div className="mt-6">
+        <Tabs defaultValue="posts">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="posts">Posts</TabsTrigger>
+            <TabsTrigger value="replies">Replies</TabsTrigger>
+            <TabsTrigger value="repost">Reposts</TabsTrigger>
           </TabsList>
+
+          <TabsContent value="posts">Posts</TabsContent>
+          <TabsContent value="replies">Replies</TabsContent>
+          <TabsContent value="repost">Repost</TabsContent>
         </Tabs>
       </div>
     </div>
