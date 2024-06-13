2970103328b94761fa64451e18a8391c27d7c4e8
Alish <alishbaig2004@gmail.com>
2024-06-13 18:52:48 +0530

    feat: Add EdgeStoreProvider to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        {children}
+        <EdgeStoreProvider>{children}</EdgeStoreProvider>
         <Toaster />
       </body>
     </html>
