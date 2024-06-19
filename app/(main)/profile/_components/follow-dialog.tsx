908a291e479e8e438da11b02251d13f70643d132
Alish <alishbaig2004@gmail.com>
2024-06-19 19:58:54 +0530

    feat: Add PostsTab component for displaying user posts in profile

---
+++ app/(main)/profile/_components/follow-dialog.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+function FollowDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="w-full">
+          Edit Dialog
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Your Circle</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <Tabs defaultValue="followers">
+            <TabsList className="w-full grid grid-cols-2">
+              <TabsTrigger value="followers">Followers</TabsTrigger>
+              <TabsTrigger value="following">Following</TabsTrigger>
+            </TabsList>
+            <TabsContent
+              value="followers"
+              className="max-h-60 px-2 my-2 overflow-y-scroll no-scrollbar"
+            >
+              {Array.from({ length: 5 }).map((_, i) => (
+                <div
+                  key={i}
+                  className="flex items-center justify-between gap-x-2 px-2 py-3 border-b"
+                >
+                  <div className="inline-flex items-center gap-x-2.5">
+                    <img
+                      src="https://api.dicebear.com/8.x/avataaars-neutral/svg?seed=Midnight"
+                      alt="avatar"
+                      className="w-8 h-8 rounded-full"
+                    />
+                    <div>
+                      <p className="text-sm">User Name</p>
+                      <p className="text-xs text-muted-foreground">Username</p>
+                    </div>
+                  </div>
+                  <Button variant="outline" size="sm" className="text-xs">
+                    Follow
+                  </Button>
+                </div>
+              ))}
+            </TabsContent>
+          </Tabs>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default FollowDialog;
