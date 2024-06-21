0e96023307c02f736ad146c4b33eca980b586409
Alish Baig <alishbaig2004@gmail.com>
2024-06-21 14:50:16 +0530

    feat: Add formatSecondsToMinutes function for formatting seconds to minutes

--- lib/format.ts
+++ lib/format.ts
@@ -27,3 +27,9 @@ export function formatDate(dateInput: Date): string {
 
   return `${dateString} at ${timeString}`;
 }
+
+export function formatSecondsToMinutes(seconds: number): string {
+  const mins = Math.floor(seconds / 60);
+  const secs = Math.floor(seconds % 60);
+  return `${mins}:${secs.toString().padStart(2, "0")}`;
+}
