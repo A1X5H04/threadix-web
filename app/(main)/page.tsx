f7c9f1f0070167c41623596e5da5e7c9f8194a9e
Alish Baig <alishbaig2004@gmail.com>
2024-06-21 14:50:39 +0530

    refactor: Add PostList component to display a list of posts on the home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import PostForm from "@/components/post-form";
 import PostItem from "@/components/post-item";
+import PostList from "@/components/post-list";
 import { posts } from "@/db/schemas/tables";
 import { validateRequest } from "@/lib/auth";
 
@@ -7,37 +8,13 @@ import axios from "axios";
 
 import useSWR from "swr";
 
-type Post = {
-  id: string;
-  content: string;
-  parentId: string;
-  userId: string;
-  createdAt: string;
-  updatedAt: string;
-};
-
 export default async function Home({}) {
   const { user } = await validateRequest();
 
-  // const { data, error, isLoading } = useSWR("/api/post", (url) =>
-  //   axios.get(url).then((res) => res.data)
-  // );
-
-  // if (error) {
-  //   return <div>Error</div>;
-  // }
-
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
-
   return (
-    <div className="space-y-5">
+    <>
       <PostForm user={user} />
-      {/* {data?.map((post: Post) => (
-      <PostItem key={post.id} data={post} />
-      ))} */}
-      Hello World
-    </div>
+      <PostList user={user} />
+    </>
   );
 }
