d2929c2e6d5611c1910633cc25414f37cdf8d0de
Alish <alishbaig2004@gmail.com>
2024-06-24 20:43:36 +0530

    refactor: Update post-content component to use PostMedia for displaying images

--- components/post-content.tsx
+++ components/post-content.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import PostVoiceRecording from "./post-voice-rec";
+import PostMedia from "./post-media";
 
 function PostContent({ content }: { content: string }) {
   return (
     <div>
       <p className="pb-2">{content}</p>
-      <PostVoiceRecording />
-      <div className="relative w-full h-72 my-2">
-        <Image
-          className="rounded-lg object-cover"
-          src="https://images.unsplash.com/photo-1542397284385-6010376c5337?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          fill
-          alt="Post Image"
-        />
-      </div>
+      <PostMedia />
     </div>
   );
 }
---
+++ components/post-media.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { RiFilmLine, RiImage2Line } from "@remixicon/react";
+import React from "react";
+import Image from "next/image";
+
+import { VoiceVisualizer, useVoiceVisualizer } from "react-voice-visualizer";
+
+function PostMedia() {
+  //   const recorderControls = useVoiceVisualizer();
+
+  return (
+    <div className="w-full">
+      {/* <div className="w-full flex justify-between items-center text-xs text-muted-foreground">
+        <span className="inline-flex items-center gap-x-2">
+          <RiFilmLine className="w-3 h-3 text-muted-foreground" />
+          <p>Shared some media</p>
+        </span>
+      </div> */}
+
+      <div className="relative w-full h-72 my-2 bg-muted rounded-lg overflow-hidden">
+        <Image
+          className="object-cover"
+          src="https://images.unsplash.com/photo-1542397284385-6010376c5337?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          fill
+          alt="Post Image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PostMedia;
+
+{
+  /* <div className="relative w-full h-72 my-2 bg-muted rounded-lg overflow-hidden">
+          <Image
+            className="object-cover"
+            src="https://images.unsplash.com/photo-1542397284385-6010376c5337?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            fill
+            alt="Post Image"
+          />
+        </div> */
+}
