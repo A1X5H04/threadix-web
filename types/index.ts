54bd00291a12f4987105733ff2bae4578200daab
Alish Baig <alishbaig2004@gmail.com>
2024-08-19 11:36:08 +0530

    refactor: Update types/index.ts to include postSchema and postMediaSchema

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import { pollSchema, voiceNoteSchema } from "./schemas";
+import { pollSchema, postMediaSchema, postSchema } from "./schemas";
 
-export type PostAudio = z.infer<typeof voiceNoteSchema>;
 export type PollSchema = z.infer<typeof pollSchema>;
+export type PostSchema = z.infer<typeof postSchema>;
+export type PostMediaSchema = z.infer<typeof postMediaSchema>;
