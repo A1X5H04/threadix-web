c84fd42b5358cf4bd9e9f66c53d2c3f86d175a13
Alish <alishbaig2004@gmail.com>
2024-09-13 18:54:39 +0530

    feat: Add initial data for post route
    This commit adds the initial data for the post route. It includes a new file `data.ts` which exports an array of post data. Each post object contains properties such as id, userId, content, likesCount, and media. This data will be used to populate the post route in the application.

---
+++ data.ts
@@ -0,0 +1,49 @@
+export const postData = [
+  {
+    id: "id126g58q6uh2jwz5crspw3u1n83nxqk",
+    userId: "vvasqs8hwjbnit2",
+    parentId: null,
+    quotePostId: null,
+    location: null,
+    content:
+      "A brand new post with images and audio, oops I cannot add audio 🤣 ",
+    mentions: null,
+    tags: null,
+    likesCount: 0,
+    repliesCount: 0,
+    repostCount: 0,
+    replyPermissions: "anyone",
+    createdAt: "2024-09-12T11:42:28.059Z",
+    updatedAt: "2024-09-12T11:42:28.061Z",
+    user: {
+      id: "vvasqs8hwjbnit2",
+      name: "John Doe",
+      username: "johndoe",
+      avatar:
+        "https://i.pinimg.com/236x/f1/97/0a/f1970a8b5bdf920a2e1977a28e2e8c77.jpg",
+      isVerified: false,
+      createdAt: "2024-06-23T16:20:52.212Z",
+    },
+    media: [
+      {
+        postId: "id126g58q6uh2jwz5crspw3u1n83nxqk",
+        name: "Blog-4-1.png",
+        url: "https://files.edgestore.dev/sst72fpj7194vcee/publicFiles/_public/4b23c5bb-91bd-4b21-a8e8-7ea282e5cd09.png",
+        width: 1543,
+        height: 867,
+        type: "image",
+        createdAt: "2024-09-12T11:42:28.180Z",
+      },
+      {
+        postId: "id126g58q6uh2jwz5crspw3u1n83nxqk",
+        name: "image (1).png",
+        url: "https://files.edgestore.dev/sst72fpj7194vcee/publicFiles/_public/e11dd9d6-7563-4fd7-8b4d-f509f4797109.png",
+        width: 2800,
+        height: 2047,
+        type: "image",
+        createdAt: "2024-09-12T11:42:28.180Z",
+      },
+    ],
+    poll: null,
+  },
+];
