e7f22a256a6e02746388a68ffddbe74c2474f4db
Alish Baig <alishbaig2004@gmail.com>
2024-11-18 12:12:38 +0530

    refactor: add useDebounceFn hook and implement modal state management in useModalStore

--- hooks/use-debounce.ts
+++ hooks/use-debounce.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState(value);
@@ -16,4 +16,30 @@ function useDebounce<T>(value: T, delay: number): T {
   return debouncedValue;
 }
 
-export default useDebounce;
+function useDebounceFn<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+) {
+  const timeoutRef = useRef<number | undefined>();
+
+  const debouncedFunc = (...args: Parameters<T>) => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    timeoutRef.current = window.setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return debouncedFunc;
+}
+
+export { useDebounce, useDebounceFn };
--- hooks/use-store.ts
+++ hooks/use-store.ts
@@ -1,1 +1,29 @@
-import { createStore } from "zustand";
+import { Post } from "@/types/api-response";
+import { create } from "zustand";
+
+interface ModalState {
+  post: {
+    post: Post | null;
+    intent?: "reply" | "quote";
+    isOpen: boolean;
+    onOpen: (post?: Post, intent?: "reply" | "quote") => void;
+    onClose: () => void;
+  };
+}
+
+export const useModalStore = create<ModalState>((set) => ({
+  post: {
+    post: null,
+    isOpen: false,
+    onOpen: (post, intent) =>
+      set((state) => ({
+        post: {
+          ...state.post,
+          isOpen: true,
+          post: post ?? null,
+          intent: intent,
+        },
+      })),
+    onClose: () => set((state) => ({ post: { ...state.post, isOpen: false } })),
+  },
+}));
