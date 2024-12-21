3344c362f85b79398abd5d948d8a32b878599861
Alish Baig <alishbaig2004@gmail.com>
2024-12-21 23:31:51 +0530

    feat: add app logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -12,7 +12,13 @@ async function AuthLayout({ children }: AuthLayoutProps) {
 
   return (
     <div className="w-full h-screen grid place-items-center bg-long-bars">
-      {children}
+      <div className="flex flex-col gap-y-10 items-center">
+        <div className="inline-flex gap-x-2 ">
+          <img src="logo.svg" width="20" />
+          <h3 className="text-xl font-extrabold tracking-tight">Threadix</h3>
+        </div>
+        {children}
+      </div>
     </div>
   );
 }
