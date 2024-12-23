d850190a0cd263eb727275341cf95ba8c6a7cddd
Alish Baig <alishbaig2004@gmail.com>
2024-12-23 23:15:34 +0530

    feat(style): adapt to theme changes and workaround for hydration errors

--- components/route-progress.tsx
+++ components/route-progress.tsx
@@ -1,14 +1,25 @@
 "use client";
+import { useState, useEffect } from "react";
 
 import { AppProgressBar } from "next-nprogress-bar";
+import { useTheme } from "next-themes";
 
 function RouteProgressBar({ children }: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
     <>
       {children}
       <AppProgressBar
-        color="#ffffff"
-        options={{ showSpinner: false }}
+        color={theme === "dark" ? "#fff" : "#000"}
+        // options={{ showSpinner: false }}
         shallowRouting
       />
     </>
